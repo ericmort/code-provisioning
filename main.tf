b9fb6780042736ce9fecce82ce9011095b638e40
Eric.Mortensen <eric.mortensen@dnb.no>
2023-12-06 09:59:27 +0100

    LKjlj

---
+++ main.tf
@@ -0,0 +1,6 @@
+//--------------------------------------------------------------------
+// Modules
+module "code_provisioning" {
+  source  = "app.terraform.io/example-org-209581/code-provisioning/no"
+  version = "1.0.1"
+}
