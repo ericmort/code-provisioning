883f42216ce63270ed742c0897c63b3af90790b2
Eric Mortensen <ericmort@users.noreply.github.com>
2023-12-11 13:45:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 //--------------------------------------------------------------------
 // Modules
+
 module "code_provisioning" {
   source  = "app.terraform.io/example-org-209581/code-provisioning/no"
   version = "1.0.2"
+  # trigger run
 }
